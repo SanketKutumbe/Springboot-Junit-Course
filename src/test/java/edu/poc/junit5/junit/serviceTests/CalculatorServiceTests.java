55970e3946e1a109ae02a9e363809d371800c90e
Sanket.Kutumbe <sanket.kutumbe@cashe.co.in>
2023-12-11 04:58:40 +0530

    Code writing flow is recommended in the form of AAA strategy (Arrange-Act-Assert). "Arrange" contains all the inputs, "Act" will act on the method under test and "Assert" will assert the result.

--- src/test/java/edu/poc/junit5/junit/serviceTests/CalculatorServiceTests.java
+++ src/test/java/edu/poc/junit5/junit/serviceTests/CalculatorServiceTests.java
@@ -12,35 +12,50 @@ public class CalculatorServiceTests {
     @DisplayName("Test 1-2=-1")
     @Test
     public void testSubstraction(){
+        // Arrange
         CalculatorService calculatorService = new CalculatorService();
         int firstNumber = 1;
         int secondNumber = 2;
-        int actualResult = calculatorService.substraction(firstNumber, secondNumber);
         int expectedResult = -1;
+
+        // Act
+        int actualResult = calculatorService.substraction(firstNumber, secondNumber);
+
+        // Assert
         assertEquals(expectedResult, actualResult);
     }
 
     @DisplayName("Test 1-2 not equal to 3")
     @Test
     public void testSubstractionWithErrorMessage(){
+        // Arrange
         CalculatorService calculatorService = new CalculatorService();
         int firstNumber = 1;
         int secondNumber = 2;
-        int actualResult = calculatorService.substraction(firstNumber, secondNumber);
         int expectedResult = 3;
         String errorMessage = "Deducting "+secondNumber+" from "+firstNumber+" is not generating expected result";
+
+        // Act
+        int actualResult = calculatorService.substraction(firstNumber, secondNumber);
+
+        // Assert
         assertEquals(expectedResult, actualResult, ()->errorMessage);
     }
 
     @DisplayName("Test 1+2=3")
     @Test
     public void testAddition_WhenTwoIntegersAreAdded_ShouldReturnThree(){
+        // Arrange
         CalculatorService calculatorService = new CalculatorService();
         int firstNumber = 1;
         int secondNumber = 2;
-        int actualResult = calculatorService.addition(firstNumber, secondNumber);
         int expectedResult = 3;
         String errorMessage = "Adding "+secondNumber+" to "+firstNumber+" is not generating expected result";
+
+        // Act
+        int actualResult = calculatorService.addition(firstNumber, secondNumber);
+
+        // Assert
         assertEquals(expectedResult, actualResult, ()->errorMessage);
     }
 }
