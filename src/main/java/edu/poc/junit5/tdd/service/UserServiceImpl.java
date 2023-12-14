40a8855d36e6d3c941dfdf127640c91fcb7507da
Sanket.Kutumbe <sanket.kutumbe@cashe.co.in>
2023-12-14 12:17:26 +0530

    Moved from TDD Lifecycle (RED) to GREEN by fixing the error.

---
+++ src/main/java/edu/poc/junit5/tdd/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package edu.poc.junit5.tdd.service;
+
+public class UserServiceImpl implements UserService {
+}
--- src/test/java/edu/poc/junit5/tdd/service/UserServiceTests.java
+++ src/test/java/edu/poc/junit5/tdd/service/UserServiceTests.java
@@ -1,11 +1,16 @@
+package edu.poc.junit5.tdd.service;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserServiceTests {
 
+    @DisplayName("Test user creation")
     @Test
     public void testCreateUser_whenUserDetailsProvided_thenCreateUser_returnCreatedUserObject(){
         // Arrange
-        UserService userService = new UserService();
+        UserService userService = new UserServiceImpl();
+
         // Act
 
         // Assert
