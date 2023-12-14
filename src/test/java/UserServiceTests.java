cbd9777d6a58e948070b0b0c45738b87bf0df9dd
Sanket.Kutumbe <sanket.kutumbe@cashe.co.in>
2023-12-14 12:10:01 +0530

    New mini-project development using TDD (Test Driven Development).

---
+++ src/test/java/UserServiceTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTests {
+
+    @Test
+    public void testCreateUser_whenUserDetailsProvided_thenCreateUser_returnCreatedUserObject(){
+        // Arrange
+        UserService userService = new UserService();
+        // Act
+
+        // Assert
+    }
+}
